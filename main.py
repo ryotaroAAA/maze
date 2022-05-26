03055769bd5050936396cd7215c1ca8f5809b68e
ryotaro <ryo.ex3991@gmail.com>
2022-05-26 20:02:10 +0900

    途中

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import pygame
+import copy
+# from dataclasses import *
+# from enum import auto, Enum
+import logging
+import logging.config
+import numpy as np
+# from pathlib import Path
+import sys
+import time
+import traceback
+# from typing import DefaultDict
+import argparse
+# from hexdump import hexdump
+from pprint import *
+# from print_color import print
+# from tabulate import tabulate
+# import yaml
+
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
@@ -14,34 +32,70 @@ flag=0
 x = 250
 y = 250
 
-while flag==0:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT: flag=1
+# @dataclass
+# class Obj:
+#     x: int = 0
+#     y: int = 0
+#     attr: ObjAttr()
 
-    press = pygame.key.get_pressed()
+#     def reset(self):
+#         self.X = 0
+#         self.Y = 0
 
-    if (press[pygame.K_LEFT] and (x - OBJ[0]) >= 0):
-        x -= OBJ[0]
+# class ObjAttr(Enum):
+#     collisionable = auto() 
+#     breakable = auto()
 
-    if (press[pygame.K_RIGHT] and
-            (x + OBJ[0]) < SCREEN[0]):
-        x += OBJ[0]
+# @dataclass
+class Hero:
+    # x: int = 0
+    # y: int = 0
+    # attr: ObjAttr()
+
+    def __init__(self):
+        self.X = 0
+        self.Y = 0
     
-    if (press[pygame.K_UP] and (y - OBJ[1]) >= 0):
-        y -= OBJ[1]
 
-    if (press[pygame.K_DOWN] and
-            (y + OBJ[1]) < SCREEN[1]):
-        y += OBJ[1]
-    
-    if (press[pygame.K_ESCAPE]):
-        break
+def run(args):
+    me = Hero()
+    flag = 0
+    while flag==0:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT: flag=1
 
-    screen.fill(WHITE)
-    rect = pygame.Rect(x, y, 10, 10)
-    pygame.draw.rect(screen, RED, rect)
-    pygame.display.flip()
-    myclock.tick(60)
+        press = pygame.key.get_pressed()
 
-pygame.quit()
+        if (press[pygame.K_LEFT] and (x - OBJ[0]) >= 0):
+            x -= OBJ[0]
 
+        if (press[pygame.K_RIGHT] and
+                (x + OBJ[0]) < SCREEN[0]):
+            x += OBJ[0]
+        
+        if (press[pygame.K_UP] and (y - OBJ[1]) >= 0):
+            y -= OBJ[1]
+
+        if (press[pygame.K_DOWN] and
+                (y + OBJ[1]) < SCREEN[1]):
+            y += OBJ[1]
+        
+        if (press[pygame.K_ESCAPE]):
+            break
+
+        screen.fill(WHITE)
+        rect = pygame.Rect(x, y, 10, 10)
+        pygame.draw.rect(screen, RED, rect)
+        pygame.display.flip()
+        myclock.tick(60)
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("-l", "--loop", type=int, default=-1, help="loop")
+    args = parser.parse_args()
+    # print(vars(args), tag="args", tag_color="green", color="white")
+    print(vars(args))
+
+    run(args)
