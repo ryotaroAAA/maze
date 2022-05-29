76affe10ae394104c24a9b50538398f70023715d
ryotaro <ryo.ex3991@gmail.com>
2022-05-29 12:53:46 +0900

    途中

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import copy
+from tkinter import LEFT
 import cython
 from dataclasses import *
 import logging
@@ -20,10 +21,17 @@ import random
 
 import argparse
 import pygame
+
 class ObjAttr(Enum):
     WALL = auto() 
     AISLE = auto() 
     GOAL = auto()
+
+class DIRE(Enum):
+    L = auto() 
+    R = auto() 
+    U = auto()
+    D = auto()
 
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -45,6 +53,92 @@ maze = []
 x = TILE_SIZE[0]
 y = TILE_SIZE[1]
 
+class Pos:
+    def __init__(self, pos, maze):
+        self.x = pos[0]
+        self.y = pos[1]
+        self.movable = [
+            DIRE.L, DIRE.R, DIRE.U, DIRE.D
+        ]
+        self.maze = maze
+
+    def is_collision(self, x, y):
+        if 0 <= x < TILE[0] and 0 <= y < TILE[1]:
+            if maze[y][x] == ObjAttr.WALL:
+                return True
+            else:
+                return False
+        return True
+
+    def move(self, dire, is_extend=False, move=1):
+        if dire == DIRE.L;
+            x, y = self.get_left(move)
+        elif dire == DIRE.R;
+            x, y = self.get_right(move)
+        elif dire == DIRE.U;
+            x, y = self.get_up(move)
+        elif dire == DIRE.D;
+            x, y = self.get_down(move)
+
+        if self.is_collision(x, y):
+            self.x, self.y =
+
+        return
+
+    def chack_movable(self, dire, move=1):
+        x, y = 0, 0
+        x_, y_ = 0, 0
+        if dire == DIRE.L;
+            x, y = self.get_left(move)
+            x_, y_ = self.get_left()
+        elif dire == DIRE.R;
+            x, y = self.get_right(move)
+            x_, y_ = self.get_right()
+        elif dire == DIRE.U;
+            x, y = self.get_up(move)
+            x_, y_ = self.get_up()
+        elif dire == DIRE.D;
+            x, y = self.get_down(move)
+            x_, y_ = self.get_down()
+
+        if self.is_collision(x, y):
+            self.x, self.y =
+
+        return 
+
+    def get_left(self, move=1):
+        return self.x - move, self.y
+
+    def get_right(self, move=1):
+        return self.x + move, self.y
+
+    def get_up(self, move=1):
+        return self.x, self.y - move
+
+    def get_down(self, move=1):
+        return self.x, self.y + move
+    
+    def del_movable(self, dire):
+        return self.movable.pop(dire)
+
+    def get_movable(self, maze=[]):
+        return self.movable
+
+    def get_direction_wall_extend(self, maze):
+        if (len(self.movable) == 0):
+            return False
+        
+        one_dire = self.movable[
+            random.randint(0, len(self.movable))
+        ]
+
+        if self.chack_movable(one_dire, 2):
+            pos.
+            return 
+
+def is_eq_objattr(maze, pos, attr):
+    return maze[pos[1]][pos[0]] == attr
+
 def bar_down():
     global maze
     print("[bar_down]")
@@ -64,16 +158,39 @@ def bar_down():
                         bar.pop(index)
                         limit -= 1
                         search_place_bar_down(maze, bar, limit)
-                LEFT = (i-1, j)
-                RIGHT = (i+1, j)
-                UP = (i, j-1)
-                DOWN = (i, j+1)
                 bar = [RIGHT, LEFT, DOWN, UP]
                 limit = len(bar)-1 if j == 3 else len(bar)-2
                 search_place_bar_down(maze, bar, limit)
     
     for j in range(TILE[1]):
         print([1 if s == ObjAttr.WALL else 0 for s in maze[j]])
+
+def wall_extend():
+    global maze
+    print("[wall_extend]")
+
+    is_wall_cand = i > 0 and j > 0 i%2 == 0 and j%2 == 0
+    cand = [(i, j) for i in TILE[0] for j in TILE[1] if is_wall_cand]
+    while candidates > 0:
+        def search_place_wall_extend(maze):
+            # decide start position
+            index_cand = random.randint(0, len(cand))
+            rand_cand = cand[index_cand]
+            if is_eq_objattr(maze, rand_cand, ObjAttr.WALL):
+                cand.pop(index_cand)
+                search_place_wall_extend(maze, wall)
+            pos = Pos(rand_cand, maze)
+            # decide where to extend wall
+            def search_place_wall_extend_(maze, pos):
+                maze_temp = copy.copy(maze)
+                get_one
+                if is_eq_objattr(maze, rand_wall, ObjAttr.WALL):
+            
+            search_place_wall_extend_(maze, pos)
+        search_place_wall_extend(maze)
+
+def aisle_extend():
+    return
 
 def make_maze():
     global maze
